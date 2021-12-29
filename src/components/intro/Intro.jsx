0b1a954b4edc99c5cbb8aa67aed0d9e4d03cad00
Salma CHAFOUK <salmachafouk14@gmail.com>
2021-12-29 20:26:04 +0100

    Intro

---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,154 @@
+import React, { useEffect, useRef } from "react"
+import { init } from "ityped"
+import { UilGithubAlt } from "@iconscout/react-unicons"
+import Me from "../../img/me.png"
+import Facebook from "../../img/facebook.png"
+import Linkedin from "../../img/linkedin.png"
+import SocialMediaArrow from "../../img/down-arrow.png"
+import "./intro.css"
+
+const Intro = () => {
+  const textRef = useRef()
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: [
+        "Web Developer",
+        "Mobile Developer",
+        "Graphic Designer",
+        "Freelancer"
+      ]
+    })
+  }, [])
+  return (
+    <div className="i">
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">Hello, My name is</h2>
+          <h1 className="i-name">Clara ARNAULT</h1>
+          <h3>
+            And I'm a <span ref={textRef}></span>
+          </h3>
+          <p className="i-desc">
+            I design and develop services for customers of all sizes,
+            specializing in creating stylish, modern websites, web services and
+            online stores.
+          </p>
+        </div>
+        <svg
+          width="75"
+          height="75"
+          viewBox="0 0 75 75"
+          fill="none"
+          stroke="black"
+          className="i-scroll"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="scroll">
+            <path
+              id="Vector"
+              d="M40.5 15L34.5 9L28.5 15"
+              strokeWidth="3"
+              strokeMiterlimit="10"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <path
+              id="Vector_2"
+              d="M28.5 24L34.5 30L40.5 24"
+              strokeWidth="3"
+              strokeMiterlimit="10"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <g id="Group">
+              <path
+                id="Vector_3"
+                d="M9 37.5H60"
+                strokeWidth="3"
+                strokeMiterlimit="10"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </g>
+            <path
+              id="Vector_4"
+              d="M34.5 27V9"
+              strokeWidth="2.9895"
+              strokeMiterlimit="10"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+            <g id="Group_2">
+              <path
+                id="Vector_5"
+                d="M9 27C9 12.918 20.418 1.5 34.5 1.5C48.5859 1.5 60 12.918 60 27C60 29.8906 60 45.1094 60 48C60 62.082 48.5859 73.5 34.5 73.5C20.418 73.5 9 62.082 9 48C9 45.1094 9 29.8906 9 27Z"
+                strokeWidth="3"
+                strokeMiterlimit="10"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </g>
+          </g>
+        </svg>
+      </div>
+      <div className="i-right">
+        <div className="i-bg"></div>
+        <img src={Me} alt="" className="i-img" />
+        <div className="social-media">
+          <div className="social-media-indicator">
+            <p>Follow</p>
+            <img src={SocialMediaArrow} alt="" />
+          </div>
+          <div className="social-media-text">
+            <ul>
+              <li>
+                <a
+                  href="https://www.facebook.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src={Facebook}
+                    alt=""
+                    style={{
+                      width: "20px",
+                      height: "20px",
+                      objectFit: "cover"
+                    }}
+                  />
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <img
+                    src={Linkedin}
+                    alt=""
+                    style={{
+                      width: "16px",
+                      height: "16px",
+                      objectFit: "cover"
+                    }}
+                  />
+                </a>
+              </li>
+              <li>
+                <a href="https://github.com" target="_blank" rel="noreferrer">
+                  <UilGithubAlt size="19" color="#111111" />
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Intro
